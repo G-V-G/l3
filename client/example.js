903371f922e305b4706dcdfa094d1b243b76dc72
Andrew1407 <endry1407@gmail.com>
2020-12-05 03:07:30 +0200

    readme desc, client (cases)

--- client/example.js
+++ client/example.js
@@ -4,14 +4,25 @@ const testData = [
   ['addForum', 'Adding a new forum (no args)', []],
   ['addForum', 'Adding a new forum (no name)', [undefined, 'golang']],
   ['addForum', 'Adding a new forum (no topic)', ['Gophers', undefined]],
+  ['addForum', 'Adding a new forum (empty name)', ['', 'Books']],
+  ['addForum', 'Adding a new forum (empty topic)', ['Gophers', '']],
+  ['addForum', 'Adding a new forum (name exists)', ['Book enjoyer', 'reading']],
+  ['addForum', 'Adding a new forum (topic exists)', ['Film Lover', 'Movies']],
   ['addForum', 'Adding a new forum', ['Gophers', 'golang']],
   ['addUser', 'Adding a new user (no args)', []],
   ['addUser', 'Adding a new user (no name)', [undefined, ['golang']]],
   ['addUser', 'Adding a new user (no interests)', ['Barbara', undefined]],
+  ['addUser', 'Adding a new user (empty string interests)', ['Barbara', ['Movies', '']]],
+  ['addUser', 'Adding a new user (empty name)', ['', ['golang']]],
+  ['addUser', 'Adding a new user (existing)', ['Bob', ['movies', 'reading']]],
   ['addUser', 'Adding a new user', ['Barbara', ['golang']]],
   ['getUser', 'Getting user (not registered)', ['Bill']],
+  ['getUser', 'Getting user (empty name)', ['']],
+  ['getUser', 'Getting user (null name)', [null]],
   ['getUser', 'Getting user', ['Barbara']],
   ['getForum', 'Getting forum (not registered)', ['Java Hell']],
+  ['getForum', 'Getting forum (empty name)', ['']],
+  ['getForum', 'Getting forum (null name)', [null]],
   ['getForum', 'Getting forum', ['Gophers']],
   ['getUsers', 'Getting all users', []],
   ['getForums', 'Getting all forums', []]
@@ -25,7 +36,7 @@ const sendTestResponses = async () => {
       console.log(comment, '\n');
       const responseFn = forumsClient[method];
       const res = await responseFn(...args);
-      console.log('Result:\n')
+      console.log('Result:')
       console.dir(res, { depth: null })
       console.log(separator)
     } catch (e) {
