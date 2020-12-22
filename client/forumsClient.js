76fe69b8379557d6d26e50059958b192fa498f6c
Andrew1407 <43030001+Andrew1407@users.noreply.github.com>
2020-12-22 16:05:21 +0200

    Update forumsClient.js

--- client/forumsClient.js
+++ client/forumsClient.js
@@ -12,7 +12,7 @@ const makeRquest = (options, responseJSON = null) => new Promise((res, rej) => {
   const responseStringified = JSON.stringify(responseJSON);
   const resData = {};
   const responseOptions = { ...connection, ...options };
-  if (method !== 'POST')
+  if (method === 'POST')
     responseOptions.headers = {
       "Content-Type": "application/json",
       "Content-Length": Buffer.byteLength(responseStringified)
